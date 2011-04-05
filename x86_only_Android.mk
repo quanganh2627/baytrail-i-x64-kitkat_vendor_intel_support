eb9e3fbd666363a5e43a2329c2c9cff768a52fa9
Bruce Beare <bruce.j.beare@intel.com>
2011-04-05 11:15:00 -0700

    x86_only_Android.mk: wasn't correctly calling all-subdir-makefiles. Changed it to call first-makefiles-under for more coverage
    Change-Id: I2b8f9dd7cbf838b616d5ff08d61f53e647d377c0
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>

--- x86_only_Android.mk
+++ x86_only_Android.mk
@@ -1,10 +1,12 @@
 #
 # file: vendor/intel/x86_only_Android.mk
-# Only compile this directory tree for IA buildsd.
+# Only compile this directory tree for IA builds.
 #
 # This makefile is copied to several places in the tree
 # by a copyfile directive in the manifest.
 
+LOCAL_PATH := $(my-dir)
+
 ifeq ($(TARGET_ARCH),x86)
-include $(all-subdir-makefiles)
+include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
