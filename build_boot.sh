8885a3856d4727857d3756cbdff4d73672fa0b7e
mark gross <mark.gross@intel.com>
2011-04-19 15:44:47 -0700

    save boot.unsigned for manual signing with other tools
    Change-Id: Ia977dbe86c619d716d7553e54d9f5d2dcf1b7783

--- build_boot.sh
+++ build_boot.sh
@@ -13,6 +13,7 @@ INITRD=out/target/product/$1/boot/ramdisk.img
 CMDLINE=out/target/product/$1/boot/cmdline
 BOOTSTUB=vendor/intel/support/bootstub
 TEMP=out/target/product/$1/tmp
+TARG=out/target/product/$1
 OUT=$2
 
 if [ "$1" == "mfld_cdk" ] || [ "$1" == "mfld_pr1" ] ;then
@@ -52,8 +53,9 @@ fi
 
 echo "Creating image \`${OUT}'for FSTK stitching..."
 
-rm -f ${TEMP}/boot.bin
-./vendor/intel/support/stitch.sh ${CMDLINE} ${BOOTSTUB} ${BZIMAGE} ${INITRD} 0 ${SILICON} ${TEMP}/boot.bin
+rm -f ${TEMP}/boot.unsigned
+./vendor/intel/support/stitch.sh ${CMDLINE} ${BOOTSTUB} ${BZIMAGE} ${INITRD} 0 ${SILICON} ${TEMP}/boot.unsigned
+cp -f ${TEMP}/boot.unsigned ${TARG}
 
 if [ "0" -ne "$?" ]; then
     echo "error running nand.sh"
@@ -62,7 +64,7 @@ fi
 TOP=`pwd`
 # stitch kboot
 cd device/intel/PRIVATE/lfstk
-./gen-os.sh ${TOP}/${TEMP}/boot.bin ${TOP}/${OUT} PAYLOADOS.XML
+./gen-os.sh ${TOP}/${TEMP}/boot.unsigned ${TOP}/${OUT} PAYLOADOS.XML
 cd  $TOP
 
 # clean up
