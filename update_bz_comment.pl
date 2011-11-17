52bc1ec5ac916a3da2d7f5381862999442bd4707
Mikael Donini <mikaelx.donini@intel.com>
2011-11-17 17:21:34 +0100

    [PORT FROM R2] make update_bz_comment.pl script compatible with 3 or 5 input parameters
    BZ: 14717
    
    make update_bz_comment.pl script compatible with 3 or 5 input parameters
    
    Change-Id: Ifc55884abaf5472b370c0da70560296199116c24
    Orig1-Change-Id: I0fad369a6b57ef6f95d951642b51682b1225e9f3
    Orig-Change-Id: I2d57cd9693802981426f8c9ba553e3520d086a21
    Signed-off-by: Mikael Donini <mikaelx.donini@intel.com>
    Reviewed-on: http://android.intel.com:8080/31902
    Reviewed-by: Monnier, OlivierX <olivierx.monnier@intel.com>
    Tested-by: Letessier, ChristopheX <christophex.letessier@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- update_bz_comment.pl
+++ update_bz_comment.pl
@@ -4,24 +4,22 @@ use Getopt::Long;
 use Term::ReadKey;
 use RPC::XML::Client;
 
-# Usage : perl update_bz_comment <filename.txt> <release_name> <daily_build_version>
-# e.g. : perl update_bz_comment info.txt MFLD_R2 20110905_002
+# Usage : perl update_bz_comment <filename.txt> <builder_name> <daily_build_version>
+# e.g. : perl update_bz_comment info.txt mikaelx MFLD_R2_20110905_002
 # This script reads a file that contains lines whose structure is as followed
 # 1234 980989 0980989 098909
 # 1234 being a BZ number and the others informations are one or more gerrit patches URL
 # The file adds the following comment in the BZ:
-# "Integrated in release MFLD_R2 build AAAAMMJJ-XXX with the patche(s): patch1 patch2 patch3\n Reporters, please verify and close"
-
-$expected_arg = 3;
+# "Integrated by builder mdoninix in build MFLD_R2_AAAAMMJJ-XXX with the patche(s): patch1 patch2 patch3\n Reporters, please verify and close"
 
 sub usage{
-  print "Usage: update_bz_comment.pl [FILE] [RELEASE_NAME] [DAILY_BUILD_VERSION]\n";
+  print "Usage: update_bz_comment.pl [FILE] [BUILDER_NAME] [DAILY_BUILD_VERSION]\n";
   print "Adds the following comment:\n";
-  print "    Integrated in release [RELEASE_NAME] build [DAILY_BUILD_VERSION] with the patche(s) PATCHx PATCHy ...\n";
+  print "    Integrated by builder [BUILDER_NAME] in build [DAILY_BUILD_VERSION] with the patche(s) PATCHx PATCHy ...\n";
   print "    Reporters, please verify and close.\n";
   print "for each BZs (first column) of FILE\n\n";
   print "EXAMPLE:\n";
-  print "    update_bz_comment.pl bz.txt MFLD_R1 20111006-001\n";
+  print "    update_bz_comment.pl bz.txt mdoninix R2_20111006-001\n";
   return 0;
 }
 
@@ -52,28 +50,35 @@ my $params = &parse_args();
 
 my $nb_arg = scalar(@ARGV);
 
-if ($nb_arg != $expected_arg) {
-  print "update_bz_comment.pl: number of ARGs=$nb_arg is NOT EQUAL to EXPECTED ARG(s)=$expected_arg\n";
+if ($nb_arg != 3 and $nb_arg != 5) {
+  print "update_bz_comment.pl: number of ARGs=$nb_arg is NOT EQUAL to EXPECTED ARG(s)=3 or 5\n";
   print "Try `update_bz_comment.pl --help' for more information.\n";
   exit 1;
 }
 
 my $inputfile = $ARGV[0];
-my $release = $ARGV[1];
+my $buildername = $ARGV[1];
 my $dailybuild = $ARGV[2];
+my $username = "";
+my $password = "";
+if ($nb_arg == 5) {
+  $username = $ARGV[3];
+  $password = $ARGV[4]; 
+}
+else {
+  ($username, $encrypted) = ( getpwuid $< )[0,1];
 
-($username, $encrypted) = ( getpwuid $< )[0,1];
+   print "Enter $username password: ";
+   ReadMode 'noecho';
+   $password = ReadLine 0;
+   chomp $password;
+   ReadMode 'normal';
 
-print "Enter $username password: ";
-ReadMode 'noecho';
-my $password = ReadLine 0;
-chomp $password;
-ReadMode 'normal';
+   print "\n";
 
-print "\n";
-
-if (crypt($password, $encrypted) ne $encrypted) {
-    die "Wrong password for $username\n";
+   if (crypt($password, $encrypted) ne $encrypted) {
+       die "Wrong password for $username\n";
+   }
 }
 
 my $cli = RPC::XML::Client->new("http://$username:$password\@umgbugzilla.sh.intel.com:41006/xmlrpc.cgi");
@@ -90,7 +95,7 @@ while (<FILE>) {
   for my $p (0 .. ($valueSize-1)) {
       if ($p == 0) {
           $BZnumber = $values[$p];
-          $string .= "Integrated in release $release build $dailybuild with the patche(s) ";
+          $string .= "Integrated by builder $buildername in build $dailybuild with the patche(s) ";
       }
       else {
           $string .= "$values[$p] ";
