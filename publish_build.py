4bf90a65d17663652bd0bd4b9a28ecaab6843c63
Marie-Cecile Afantenos <marie.cecile.afantenos@intel.com>
2013-07-03 12:54:31 +0200

    publish: remove obsolete platform
    BZ: 120735
    
    on redhookbay ctp_vb_b0b1 and ctp_vv are osolete, remove them
    Signed-off-by: Marie-Cecile Afantenos <marie.cecile.afantenos@intel.com>
    
    Change-Id: I4553837a1ead7d93ef0d9ae9f04183f537eb4385
    Reviewed-on: http://android.intel.com:8080/116789
    Reviewed-by: Muller, Francois-nicolas <francois-nicolas.muller@intel.com>
    Reviewed-by: Betous, GuillaumeX <guillaumex.betous@intel.com>
    Reviewed-by: Afantenos, Marie Cecile <marie.cecile.afantenos@intel.com>
    Reviewed-by: Lumbierres, PhilippeX <philippex.lumbierres@intel.com>
    Reviewed-by: Seibel, Eric <eric.seibel@intel.com>
    Tested-by: Molero Rodenas, BeatrizX <beatrizx.molero.rodenas@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- publish_build.py
+++ publish_build.py
@@ -85,8 +85,8 @@ def find_ifwis(basedir):
                     "salitpa":"salitpa",
                     "yukkabeach":"yukkabeach",
                     "victoriabay":"victoriabay vb_vv_b0_b1 vb_vv vb_pr1-01 vb_pr1",
-                    "redhookbay":"ctp_pr[23] ctp_pr3.1 ctp_vv2 ctp_vv_b0_b1 ctp_vv3 ctp_vv",
-                    "redhookbay_next":"ctp_pr[23] ctp_pr3.1 ctp_vv2 ctp_vv_b0_b1 ctp_vv3 ctp_vv",
+                    "redhookbay":"ctp_pr[23] ctp_pr3.1 ctp_vv2 ctp_vv3",
+                    "redhookbay_next":"ctp_pr[23] ctp_pr3.1 ctp_vv2 ctp_vv3",
                     "ctp7160":"vb_vv_b0_b1",
                     "ctpscalelt":"ctp_vv2/CTPSCALELT",
                     "saltbay_lnp":"saltbay_pr1 saltbay_pr1/DBG saltbay_pr1/PSH",
@@ -458,17 +458,6 @@ def publish_blankphone(basedir, bld, buildnumber):
 
         f.finish()
 
-	# TEMPORARY MODIFICATION FOR BZ 9642 INTEGRATION
-	# TO BE REMOVED ONCE NEW IFWI MAPPING IS TOTALLY MERGED
-
-	# Keep compatibility with ACS and keep old blankphone names
-	if board == "ctp_vv_b0_b1":
-		shutil.copyfile(os.path.join(blankphone_dir, "ctp_vv_b0_b1-blankphone.zip"), os.path.join(blankphone_dir, "ctp_vv2-blankphone.zip"))
-	if board == "ctp_vv":
-		shutil.copyfile(os.path.join(blankphone_dir, "ctp_vv-blankphone.zip"), os.path.join(blankphone_dir, "ctp_vv3-blankphone.zip"))
-	#
-	# END
-	#
 
 def publish_modem(basedir, bld):
     # environment variables
