0575ed14319f1d6d6a45ccd9c1036bf46035fc0e
Jocelyn Falempe <jocelyn.falempe@intel.com>
2013-11-04 16:07:25 +0100

    Adding new target byt_t_crv2
    BZ: 149053
    
    publish_build.py is not scalable, and require tweaks when adding
    a new target.
    There are no ifwis for byt_t_crv2
    
    Change-Id: Ibd35ffa9f060e8abf08dc066316f242c5fdea10e
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>

--- publish_build.py
+++ publish_build.py
@@ -115,7 +115,7 @@ def find_ifwis(basedir, board_soc):
         ifwi_base_dir = ifwi_external_dir
     # IFWI for Merrifield/Moorefield VP, HVP and SLE are not published
     # Filter on _vp, _vp_next, _hvp, _hvp_next, _sle, _sle_next
-    isvirtualplatorm = re.match('.*_(vp|hvp|sle)($|\s|_next($|\s))', bld_prod)
+    isvirtualplatorm = re.match('.*_(vp|hvp|sle|crv2)($|\s|_next($|\s))', bld_prod)
     if not(isvirtualplatorm):
         ifwiglobs = {"redhookbay": "ctp_pr[23] ctp_pr3.1 ctp_vv2 ctp_vv3",
                      "redhookbay_next": "ctp_pr[23] ctp_pr3.1 ctp_vv2 ctp_vv3",
