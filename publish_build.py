a5bf8ff3036ef03876416d6b1c5b52b0a125c376
Sylvain Chouleur <sylvain.chouleur@intel.com>
2014-02-13 14:51:31 +0100

    [REVERTME] Wipe reserved partition in UEFI blankphone
    BZ: 170234
    
    A partitioning layout change has moved ESP position without wiping its
    content.
    The result is that UEFI BIOS found the old ESP FS and use it instead of
    the new one.
    
    This patch is used to clear all the boards that have the residual ESP
    and will have to be reverted once it's done.
    
    Change-Id: Ia0c2403bd1814620f4e115704a9e21dc2503066d
    Signed-off-by: Sylvain Chouleur <sylvain.chouleur@gmail.com>

--- publish_build.py
+++ publish_build.py
@@ -656,6 +656,7 @@ def publish_blankphone_uefi(bld, buildnumber, board_soc):
     f.add_command("sleep", "Sleep for 25 seconds.", timeout=25000)
 
     f.add_command("fastboot oem wipe ESP", "Wiping ESP partition.", mandatory=0);
+    f.add_command("fastboot oem wipe reserved", "Wiping reserved partition.", mandatory=0);
     publish_partitioning_commands(f, bld, buildnumber, os.path.split(part_file)[1],
                                   ["system", "cache", "config", "logs", "data"])
 
